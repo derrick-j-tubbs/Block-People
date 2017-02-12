93a34aadf59ed38e2f484af43a3fff0c18c961b7
derrick-j-tubbs <derrick.j.tubbs@gmail.com>
2017-02-12 00:03:40 -0500

    Update

---
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerCollisions : MonoBehaviour
+    {
+        private Player playerScript;
+        public LayerMask floorLayer;
+
+        void Start()
+        {
+            floorLayer = LayerMask.NameToLayer("Floor");
+            playerScript = GetComponent<Player>();
+        }
+
+        void OnCollisionEnter2D(Collision2D coll)
+        {
+            if (gameObject.tag == "Player" && coll.gameObject.layer == floorLayer)
+            {
+                playerScript.bGrounded = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D coll)
+        {
+            if (gameObject.tag == "Player" && coll.gameObject.layer == floorLayer)
+            {
+                playerScript.bGrounded = false;
+            }
+        }
+    }
+}
